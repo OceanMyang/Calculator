a8d54ae9941d346049f5034c4409c417fc7a626c
OceanZ <my18702189114@gmail.com>
2024-05-23 19:52:33 -0400

    tests

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    //a-z 97 - 122
+    //0-9 48 - 57
+    char c = '0';
+    System.out.println(c + 0);
+  }
+}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package calculator;
+
+public interface Calculator {
+  Calculator input(char c);
+  String getResult();
+}
