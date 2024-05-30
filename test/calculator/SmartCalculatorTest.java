c573c1082888f28da270debce43e7853e9fef783
OceanZ <my18702189114@gmail.com>
2024-05-30 11:09:14 -0400

    SmartCalculator Tests

---
+++ test/calculator/SmartCalculatorTest.java
@@ -0,0 +1,46 @@
+package calculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+public class SmartCalculatorTest extends CalculatorTest {
+
+  @Before
+  public void setUp() {
+    calculator = new SmartCalculator();
+  }
+
+  @Test
+  public void multipleEqualSigns() {
+    equation = "32+24=";
+    assertEquals("56", execute(equation));
+    equation = "32+24==";
+    assertEquals("80", execute(equation));
+    equation = "32+24===";
+    assertEquals("104", execute(equation));
+  }
+
+  @Test
+  public void skipSecondOperand() {
+    equation = "32+=";
+    assertEquals("64", execute(equation));
+    equation = "32+==";
+    assertEquals("96", execute(equation));
+  }
+
+  @Test
+  public void consecutiveOperators() {
+    equation = "32+-24=";
+    assertEquals("8", execute(equation));
+  }
+
+  @Test
+  public void beginWithOperator() {
+    equation = "+32-24=";
+    assertEquals("8", execute(equation));
+  }
+}
